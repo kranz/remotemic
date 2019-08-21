1b495b0a27ef613fb96c502a5e721d9e9be907ac
kranz <francescpellegrini@gmail.com>
2019-08-21 15:09:35 +0200

    tentativo gestione da console

--- client.js
+++ client.js
@@ -3,6 +3,10 @@
 const encoding = 'LINEAR16';
 const sampleRateHertz = 16000;
 const languageCode = 'it-IT';
+var standard_input = process.stdin;
+
+// Set input character encoding.
+standard_input.setEncoding('utf-8');
 
 
 function microphoneStream(encoding, sampleRateHertz, languageCode) {
@@ -11,26 +15,39 @@ function microphoneStream(encoding, sampleRateHertz, languageCode) {
   const fs = require('fs')
  
   const file = fs.createWriteStream('test.wav', { encoding: 'binary' })
-
   const ws = new WebSocket('ws://192.168.254.161:8080');
-  recorder.record({
-      sampleRateHertz: sampleRateHertz,
-      threshold: 0, //silence threshold
-      recordProgram: 'rec', // Try also "arecord" or "sox"
-      silence: '1.0', //seconds of silence before ending
+
+  standard_input.on('data', function(data) {
+    switch(data) 
+    {
+      case 'r\n':
+        console.log('Listening, s + Enter to stop.');
+        recorder.record({
+          sampleRateHertz: sampleRateHertz,
+          threshold: 0, //silence threshold
+          recordProgram: 'rec', // Try also "arecord" or "sox"
+          silence: '1.0', //seconds of silence before ending
+        })
+        .stream()
+        .on('error', console.error)
+        .pipe(ws);
+      break;
+      case 's\n':
+        console.log('Stop recording');
+        recorder.stop();
+      break;
+      case 'q\n':
+        recorder.stop();
+        ws.close();
+        console.log('Stop recording and transmitting. BYE');
+        process.exit();
+      break;
+      default:
+        console.log('...zzo vuoi?');
+      break;
+    }
   })
-  .stream()
-  .on('error', console.error)
-  .pipe(ws);
-  
-  console.log('Listening, press Ctrl+C to stop.');
-  
-  
-//  ws.on('open', function open(data,request) {
-//    ws.send(data);
-//  });
-  
-  
+
   ws.socket.on('message', function incoming(data) {
     console.log(data);
   });
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const WebSocket = require('websocket-stream');
 const fs = require ('fs');
 var stream = require('stream');
 
-const wss = WebSocket.createServer({host: '192.168.254.161', port: 8080},handle);
+const wss = WebSocket.createServer({host: '192.168.254.161', port: 8090},handle);
 
 function handle(stream)  {
 	var target = fs.createWriteStream('testalo.wav', { encoding: 'binary' });
 	stream.pipe(target);	
   stream.on('close', function (){
+    target.end();
+  }).on('error', function() {
     target.end();
   })
 }
@@ -17,12 +19,12 @@ var response = "vanni a pigiâlo into cû ";
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-	console.log(message);
-  	console.log('ricevuti %s bytes', message.length);
+	 	console.log('ricevuti %s bytes', message.length);
     console.log('sent back: %s', message.length);
   	ws.send(`${response} ${message.length} vòtte`);
 	  
   }).on('close', function () {
+    console.log("FINITO")
   	
   }).on('error',function (err) {
     console.log("error:",err)
