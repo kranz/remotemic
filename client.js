d6f296607cf008d49794165affa1a7f861b9c011
kranz <francescpellegrini@gmail.com>
2019-08-21 16:31:50 +0200

    gestione stop/restart del flusso audio

--- client.js
+++ client.js
@@ -15,35 +15,44 @@ function microphoneStream(encoding, sampleRateHertz, languageCode) {
   const fs = require('fs')
  
   const file = fs.createWriteStream('test.wav', { encoding: 'binary' })
-  const ws = new WebSocket('ws://192.168.254.161:8090');
+  console.log('Inserire r+INVIO per registrare, s + INVIO to stoppare, q + INVIO per terminare.');
 
-  standard_input.on('data', function(data) {
-    switch(data) 
-    {
-      case 'r\n':
-        console.log('Listening, s + Enter to stop.');
-        recorder.record({
+  var recording = recorder.record({
           sampleRateHertz: sampleRateHertz,
           threshold: 0, //silence threshold
           recordProgram: 'sox', // Try also "arecord" or "sox"
           silence: '1.0', //seconds of silence before ending
         })
-        .stream()
-        .on('error', console.error)
-        .pipe(ws);
+
+  var ws = new WebSocket('ws://192.168.254.161:8090');
+  standard_input.on('data', function(data) {
+    switch(data) 
+    {
+      case 'r\n':
+        console.log('Listening, s + Enter to stop.');
+        recording = recorder.record({
+           sampleRateHertz: sampleRateHertz,
+           threshold: 0, //silence threshold
+           recordProgram: 'sox', // Try also "arecord" or "sox"
+           silence: '1.0', //seconds of silence before ending
+        })
+        ws = new WebSocket('ws://192.168.254.161:8090');
+        recording
+          .stream()
+          .on('error', console.error)
+         .pipe(ws);
       break;
       case 's\n':
         console.log('Stop recording');
-        recorder.stop();
+        recording.stop();
       break;
       case 'q\n':
-        recorder.stop();
-        ws.close();
+        recording.stop();
         console.log('Stop recording and transmitting. BYE');
         process.exit();
       break;
       default:
-        console.log('...zzo vuoi?');
+        console.log('HAI SCRITTO <'+ data + '>??? ...zzo vuoi?');
       break;
     }
   })
